9faf5ac98aeab62a2b4c5b6798c6bd4d5c012f11
jayecook <jayecrook@gmail.com>
2026-03-27 17:43:32 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductManager from "./ProductManager";
+
+function App() {
+  return (
+    <div>
+      <ProductManager />
+    </div>
+  );
+}
+
+export default App;
