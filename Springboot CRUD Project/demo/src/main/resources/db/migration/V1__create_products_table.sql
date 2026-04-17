22971f352ce73838f7d6db902102fe2d21c32071
zkelani <mogbojubolak@gmail.com>
2026-04-17 17:39:32 -0500

    Add count column to products table creation

--- Springboot CRUD Project/demo/src/main/resources/db/migration/V1__create_products_table.sql
+++ Springboot CRUD Project/demo/src/main/resources/db/migration/V1__create_products_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE if not exists products(
     id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     description varchar(255),
+    count int,
     price Float,
     product_type varchar(255)
 );
