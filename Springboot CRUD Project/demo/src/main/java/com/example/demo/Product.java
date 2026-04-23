1ba5a1980ab89ab42c9477f35f582f817bd49fd9
Mircea <luminol7777@gmail.com>
2026-04-22 21:43:14 -0500

    Quick fix to prior push

--- Springboot CRUD Project/demo/src/main/java/com/example/demo/Product.java
+++ Springboot CRUD Project/demo/src/main/java/com/example/demo/Product.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
